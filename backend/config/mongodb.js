38136752b0e19ad1d28921bc8f8d5b2aae349b13
Jiyanshu-Sharma-meridian <jiyanshu.sharma@onmeridian.com>
2025-12-15 12:01:30 +0530

    Initial commit

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
+import 'dotenv/config';
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB Connected");
   });
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+  await mongoose.connect(`${process.env.MONGO_URI}/e-commerce`);
+  //await mongoose.connect(process.env.MONGODB_URI);
 };
+
 
 export default connectDB;
